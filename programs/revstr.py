2248b915818ae6fffc49d3b803566b30e5930867
Aishwarya Sebin <aishsebin@gmail.com>
2019-05-31 18:36:54 +0530

    program 2 updated

--- programs/revstr.py
+++ programs/revstr.py
@@ -1,19 +1,69 @@
 #problem 2: program to reverse string wrt words
 
-str=input("Enter string: ")
-data=str.split(" ")
-# print(data)
 
-while '' in data:
-    data.remove('')
+ 
+# # data=str.split(" ")
+# # print(data)
 
-# print(data)
+# while '' in data:
+#     data.remove('')
 
-x=len(data)-1
-revstr=data[x]
+# # print(data)
 
-while(x>0):
-    x-=1
-    revstr=revstr+" "+data[x]
+# x=len(data)-1
+# revstr=data[x]
+
+# while(x>0):
+#     x-=1
+#     revstr=revstr+" "+data[x]
     
+# print("Reverse str:",revstr)
+
+revstr=""
+stack=[]
+
+# global i
+i=-1
+def push(x):
+    global i
+    global stack
+    # if(i==-1):
+    #     i=0
+    # else:
+    i+=1
+  
+    stack+=[x]
+    
+    
+def pop():
+    global i
+    global stack
+    x=stack[i]
+    stack=stack[:i]
+    i-=1
+    return x
+
+
+def isempty():
+    global i
+    if(i==-1):
+        return 1
+    else:
+        return 0
+
+
+str=input("Enter string: ")
+j=len(str)-1
+while(j>=0):
+    if(str[j]!=' '):
+        push(str[j])
+    else:
+        if(isempty()==0):
+            while(isempty()==0):
+                revstr=revstr+pop()
+            revstr+=" "
+    j-=1 
+
+while(isempty()==0):
+    revstr=revstr+pop()
 print("Reverse str:",revstr)
